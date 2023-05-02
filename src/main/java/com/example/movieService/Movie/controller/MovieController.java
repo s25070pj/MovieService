22e4c25ca3a3047cb52744a2d8d7a83abfa78590
brash <s25070@pjwstk.edu.pl>
2023-05-02 21:04:01 +0200

    Creation of the MovieController, injecting dependencies from MovieService and creating "/movies" endpoint

---
+++ src/main/java/com/example/movieService/Movie/controller/MovieController.java
@@ -0,0 +1,32 @@
+package com.example.movieService.Movie.controller;
+
+import com.example.movieService.Movie.Model.Movie;
+import com.example.movieService.Movie.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/app")
+public class MovieController {
+
+    private final MovieService movieService;
+    @Autowired
+    public MovieController(MovieService movieService){
+        this.movieService = movieService;
+    }
+
+    @GetMapping("/movies")
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        if (!movieService.getAllMovies().isEmpty()) {
+            return ResponseEntity.ok(movieService.getAllMovies());
+        } else {
+            return  ResponseEntity.noContent().build(); //if list is empty - 204
+        }
+    }
+
+}
